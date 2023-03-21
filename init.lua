71bea08b46588980d2154924c59882a0e9a0a2df
flux <25628292+fluxionary@users.noreply.github.com>
2023-03-21 16:17:11 -0700

    preliminary status effects integration

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@ balanced_nutrient_overrides = fmod.create()
 
 function balanced_nutrient_overrides.register_food(name, def)
 	if not minetest.registered_items[name] then
-		-- TODO: warning?
+		balanced_nutrient_overrides.log("warning", "%s doesn't exist, skipping override", name)
 		return
 	end
 	balanced_diet.register_food(name, def)
@@ -12,7 +12,12 @@ end
 
 function balanced_nutrient_overrides.effects(to_add, to_remove)
 	return function(itemstack, eater, pointed_thing)
-		-- TODO: integrate this w/ status_effects
+		--for effect_name, value in pairs(to_add) do
+		--	local effect = std_effects[effect_name]
+		--	if effect then
+		--		effect:add_time(eater, value[1], value[2], "balanced_nutrient_overrides")
+		--	end
+		--end
 	end
 end
 
