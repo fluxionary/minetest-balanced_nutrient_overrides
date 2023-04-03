4435cd93f55b30202b693b4d007b6581af7a0f01
flux <25628292+fluxionary@users.noreply.github.com>
2023-04-03 14:49:12 -0700

    aotearoa

---
+++ aotearoa.lua
@@ -0,0 +1,161 @@
+balanced_nutrient_overrides.register_food("aotearoa:cockle", {
+	-- item_eat(2)
+	saturation = 2,
+	duration = 500,
+	nutrients = {
+		fat = 1,
+		protein = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_bracken_root", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_cabbage_tree_root", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_cabbage_tree_shoots", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_fiddlehead", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_mamaku_pith", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_nikau_shoots", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:cooked_raupo_root", {
+	-- item_eat(1)
+	saturation = 2,
+	duration = 600,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:hinau_fruit", {
+	-- item_eat(1)
+	saturation = 1,
+	duration = 300,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 1,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:karaka_fruit", {
+	-- item_eat(1)
+	saturation = 1,
+	duration = 300,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 1,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:kawakawa_fruit", {
+	-- item_eat(1)
+	saturation = 1,
+	duration = 300,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 1,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:miro_fruit", {
+	-- item_eat(1)
+	saturation = 1,
+	duration = 300,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 1,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:pipi", {
+	-- item_eat(2)
+	saturation = 2,
+	duration = 500,
+	nutrients = {
+		fat = 1,
+		protein = 3,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:pungapunga", {
+	-- item_eat(8)
+	saturation = 4,
+	duration = 1000,
+	nutrients = {
+		carbohydrate = 8,
+		fat = 2,
+		vitamin = 2,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:seed_cake", {
+	-- item_eat(8)
+	saturation = 4,
+	duration = 1000,
+	nutrients = {
+		carbohydrate = 8,
+		fat = 2,
+		vitamin = 2,
+	},
+})
+
+balanced_nutrient_overrides.register_food("aotearoa:tawa_fruit", {
+	-- item_eat(1)
+	saturation = 1,
+	duration = 300,
+	nutrients = {
+		carbohydrate = 1,
+		vitamin = 1,
+	},
+})
